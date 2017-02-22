2988a4d545cb87482dd3ef1d2c1adcaa135dd73d
Lazy_guy <michael251902523@gmail.com>
2017-02-22 22:52:33 +0800

    hands on
    hands on the code

---
+++ asgn2/python_code/task1/initial_code.py
@@ -0,0 +1,6 @@
+print 'the connect-4 game begins !'
+row1=row2=row3=row4=row5=row6=['O','O','O','O','O','O','O']
+game_board = [row1,row2,row3,row4,row5,row6]
+for row in game_board:
+	print row[0:]
+print game_board
---
+++ asgn2/python_code/task1/try_out_python.py
@@ -0,0 +1,8 @@
+# //print("here is the clipboard display")
+print "Hello new way"
+str = "what the hell"
+print str[2:5]*2+"!test"
+list = ['a','b','c','d','e','f','g',1,2,3,4,5,6,7]
+small_list = ['the new list']
+print list[3:5]
+print list[3:]*2+small_list
