8d1b5d4fe555bede854f04271a57462ead546eb1
Lazy_guy <michael251902523@gmail.com>
2017-02-23 16:37:10 +0800

    Change to the class structure
    change to the class structure

--- asgn2/python_code/task1/initial_code.py
+++ asgn2/python_code/task1/initial_code.py
@@ -1,20 +1,3 @@
-class Connect_Four:
-    'Descriptor : The connect four playboard'
-    row1=row2=row3=row4=row5=row6=[' ',' ',' ',' ',' ',' ',' ']
-    game_board = [row1,row2,row3,row4,row5,row6]
-    def __init__(self):
-        print 'a new Connect Four is created'
-    def StartGame(self):
-        print 'this is a new game !'
-    def PrintGameBoard(self):
-        print '| 1 | 2 | 3 | 4 | 5 | 6 | 7 |'
-        print '-----------------------------'
-        for row in self.game_board:
-            print '| %c | %c | %c | %c | %c | %c | %c |' % (row[0],row[1],row[2],row[3],row[4],row[5],row[6])
-            print '-----------------------------'
-	
-	
-
 class Player:
     'Descriptor : The general player class, human player and AI player are subclasses of this player class'
     playerSymbol = 'O'
@@ -26,6 +9,42 @@ class Player:
     def ShowPlayerType(self):
         print 'the player is ', self.playerSymbol
 
+
+class Connect_Four:
+    'Descriptor : The connect four playboard'
+    row1=row2=row3=row4=row5=row6=[' ',' ',' ',' ',' ',' ',' ']
+    gameboard = [row1,row2,row3,row4,row5,row6]
+    player1 = Player('O')
+    player2 = Player('X')
+    turn = 1
+    def __init__(self):
+        print 'a new Connect Four is created'
+    def ChoosePlayer(self):
+        #choose fist player
+        print 'Please choose the first player:\n1.Computer\n2.Human'
+        input_str=raw_input('Your choice is:')
+        if(input_str[0]=='1'):
+            print 'Player O is Computer'
+        if(input_str[0]=='2'):
+            print 'Player O is Human'
+        #choose seconde player
+        print 'Please choose the second player:\n1.Computer\n2.Human'
+        input_str=raw_input('Your choice is:')
+        if(input_str[0]=='1'):
+            print 'Player X is Computer'
+        if(input_str[0]=='2'):
+            print 'Player X is Human'
+
+    def StartGame(self):
+        print 'this is a new game !'
+        self.ChoosePlayer()
+    def PrintGameBoard(self):
+        print '| 1 | 2 | 3 | 4 | 5 | 6 | 7 |'
+        print '-----------------------------'
+        for row in self.gameboard:
+            print '| %c | %c | %c | %c | %c | %c | %c |' % (row[0],row[1],row[2],row[3],row[4],row[5],row[6])
+            print '-----------------------------'
+	
 
 print Connect_Four.__doc__
 #create and test Connect_Four class
