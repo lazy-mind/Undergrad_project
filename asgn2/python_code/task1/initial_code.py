6a8e1c8ac378a35046015692f14d986787e3004c
Lazy_guy <michaelmao@MichaelMaodeMacBook-Air.local>
2017-03-05 15:06:20 +0800

    Basic user interface is complete
    remain the algorithm of AI to be implemented

--- asgn2/python_code/task1/initial_code.py
+++ asgn2/python_code/task1/initial_code.py
@@ -4,15 +4,38 @@ class Player:
     def __init__(self, PlayerSymbol):
         self.playerSymbol = PlayerSymbol
     def NextColumn(self, gameBoard):
-        print 'the next column is shown to choose'
+        print ('here')
+        pass
+    # the NextColumn function is to be override by the subclasses
     def ShowPlayerType(self):
         print 'the player is ', self.playerSymbol
 
 class Human(Player):
-    pass
+    def HumanThinking(self,gameBoard):
+        while(1):
+            input_str=raw_input('Please choose a column:')
+            if(input_str.isdigit()):
+                column_index=int(input_str)
+                if (column_index>=1 and column_index<=7):
+                    if (gameBoard.gameboard[0][column_index-1]==' '):
+                        gameBoard.UpdateGameBoard(column_index-1)
+                        break;
+                    else:
+                        print ('invalid input column!')
+                else:
+                    print ('invalid input column!')
+            else:
+                print ('invalid input string!')
+    def NextColumn(self, gameBoard):
+        print ('human thinking')
+        self.HumanThinking(gameBoard)
 
 class Computer(Player):
-    pass
+    def ComputerThinking(self,gameBoard):
+        pass
+    def NextColumn(self, gameBoard):
+        print ('AI thinking')
+        self.ComputerThinking(gameBoard)
 
 class Connect_Four:
     'Descriptor : The connect four playboard'
@@ -29,27 +52,40 @@ class Connect_Four:
         while(1):
             print 'Please choose the first player:\n1.Computer\n2.Human'
             input_str=raw_input('Your choice is:')
-            if(input_str[0]=='1'):
-                print 'Player O is Computer'
-                break
-            elif(input_str[0]=='2'):
-                print 'Player O is Human'
-                break
+            if(input_str.isdigit()):
+                input_num=int(input_str)
+                if(input_num==1):
+                    self.player1=Computer('O')
+                    print 'Player O is Computer'
+                    break
+                elif(input_num==2):
+                    self.player1=Human('O')
+                    print 'Player O is Human'
+                    break
+                else:
+                    print 'invalid player type!'
             else:
                 print 'invalid player type!'
-
+                
+    
         #choose seconde player
         while(1):
             print 'Please choose the second player:\n1.Computer\n2.Human'
             input_str=raw_input('Your choice is:')
-            if(input_str[0]=='1'):
-                print 'Player X is Computer'
-                break
-            elif(input_str[0]=='2'):
-                print 'Player X is Human'
-                break
+            if(input_str.isdigit()):
+                input_num=int(input_str)
+                if(input_num==1):
+                    self.player2=Computer('X')
+                    print 'Player X is Computer'
+                    break
+                elif(input_num==2):
+                    self.player2=Human('X')
+                    print 'Player X is Human'
+                    break
+                else:
+                    print 'invalid player type!'
             else:
-                print 'invalid player type!'
+                print ('invalid player type!')
 
     def StartGame(self):
         print 'this is a new game !'
@@ -60,15 +96,54 @@ class Connect_Four:
         for row in self.gameboard:
             print '| %c | %c | %c | %c | %c | %c | %c |' % (row[0],row[1],row[2],row[3],row[4],row[5],row[6])
             print '-----------------------------'
+    def UpdateGameBoard(self,column_index):
+        print 'I am not updated'
+        self.PrintGameBoard()
+    def IsGameOver(self):
+        #check row
+        for each_row in self.gameboard:
+            count=1
+            for index in range(1,7):
+                if(each_row[index-1]==' '):
+                    count=1
+                elif(each_row[index]==each_row[index-1]):
+                    count=count+1
+                else:
+                    count=1
+                if(count==4):
+                    print ('row')
+                    return 1
+        #check column
+        for column_index in range(0,7):
+            count=1
+            for row_index in range(1,6):
+                if(self.gameboard[row_index-1][column_index]==' '):
+                    count=1
+                elif(self.gameboard[row_index-1][column_index]==self.gameboard[row_index][column_index]):
+                    count=count+1
+                else:
+                    count=1
+                if(count==4):
+                    print ('col')
+                    return 1
+        #check diagnal:
+        return 0
 	
 
-print Connect_Four.__doc__
+#print Connect_Four.__doc__
 #create and test Connect_Four class
 A_Game = Connect_Four()
 A_Game.StartGame()
 A_Game.PrintGameBoard()
-#create and test Player class
-Human_Player = Player('O')
-Human_Player.ShowPlayerType()
-AI_Player = Player('X')
-AI_Player.ShowPlayerType()
+while(A_Game.IsGameOver()==0):
+    print('next turn')
+    if(A_Game.turn==1):
+        #print ('turn is 1')
+        A_Game.player1.NextColumn(A_Game)
+        A_Game.turn=0
+    else:
+        #print ('turn is 0')
+        A_Game.player2.NextColumn(A_Game)
+        A_Game.turn=1
+print('end of the game')
+
